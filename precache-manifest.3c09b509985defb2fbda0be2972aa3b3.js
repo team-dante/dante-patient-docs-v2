36e39d9205627462cd7fd9910993de8a46164022
Xinhao Liang <xinhao128@hotmail.com>
2019-12-26 22:32:01 -0800

    Updates

---
+++ precache-manifest.3c09b509985defb2fbda0be2972aa3b3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2ace81ee8c8fbbb71665f091aa8d26",
+    "url": "/dante-patient-docs-v2/index.html"
+  },
+  {
+    "revision": "666b795387dca7efeaa8",
+    "url": "/dante-patient-docs-v2/static/css/main.3f975176.chunk.css"
+  },
+  {
+    "revision": "d6489d5b9d591568c55f",
+    "url": "/dante-patient-docs-v2/static/js/2.e9417d49.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dante-patient-docs-v2/static/js/2.e9417d49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "666b795387dca7efeaa8",
+    "url": "/dante-patient-docs-v2/static/js/main.21c8c4dc.chunk.js"
+  },
+  {
+    "revision": "1cfe1376b3773e63c25c",
+    "url": "/dante-patient-docs-v2/static/js/runtime-main.ddbc7a4f.js"
+  },
+  {
+    "revision": "c60adf59ca1277bc3aa0a66596942f5d",
+    "url": "/dante-patient-docs-v2/static/media/Date.c60adf59.png"
+  },
+  {
+    "revision": "d82278e95196e1ebddfc70e3aab07f78",
+    "url": "/dante-patient-docs-v2/static/media/Graph.d82278e9.png"
+  },
+  {
+    "revision": "c28869337aa22ee0b1d503639094d6f8",
+    "url": "/dante-patient-docs-v2/static/media/LocationTracking.c2886933.png"
+  },
+  {
+    "revision": "916631fdee34dc2780d6febd03b1ce99",
+    "url": "/dante-patient-docs-v2/static/media/SignIn.916631fd.PNG"
+  },
+  {
+    "revision": "035757b4bc4355c98474479a06046f69",
+    "url": "/dante-patient-docs-v2/static/media/TimeTracking.035757b4.jpg"
+  },
+  {
+    "revision": "dd6e6428a666b36c0c11069277081d43",
+    "url": "/dante-patient-docs-v2/static/media/by-day.dd6e6428.jpg"
+  },
+  {
+    "revision": "53c1a04dec9334b9b424ab5bd4b00f3c",
+    "url": "/dante-patient-docs-v2/static/media/by-month.53c1a04d.jpg"
+  },
+  {
+    "revision": "32320d356bb41da8cb145704fd39f896",
+    "url": "/dante-patient-docs-v2/static/media/by-week.32320d35.jpg"
+  },
+  {
+    "revision": "5c2957ffecb8b3b69218fe8729c8e403",
+    "url": "/dante-patient-docs-v2/static/media/check-in-procedure.5c2957ff.jpg"
+  },
+  {
+    "revision": "59a46f9ece426e15b317fdff4c3e2d95",
+    "url": "/dante-patient-docs-v2/static/media/check-out-procedure.59a46f9e.jpg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/dante-patient-docs-v2/static/media/close-default.777b93c6.svg"
+  },
+  {
+    "revision": "c10f3ab8d09dcd1ca9a9bfe14b4ebffc",
+    "url": "/dante-patient-docs-v2/static/media/close.c10f3ab8.svg"
+  },
+  {
+    "revision": "85d5e7321e18074e2d04ef7de9dc970a",
+    "url": "/dante-patient-docs-v2/static/media/dante-patient-logo.85d5e732.png"
+  },
+  {
+    "revision": "c2219c0f278500f361206edada36779a",
+    "url": "/dante-patient-docs-v2/static/media/dante_staff.c2219c0f.jpg"
+  },
+  {
+    "revision": "86755724474762d2665c2f0760003654",
+    "url": "/dante-patient-docs-v2/static/media/fab.86755724.svg"
+  },
+  {
+    "revision": "522d5cc49a13ef329be73c8784b2bc2a",
+    "url": "/dante-patient-docs-v2/static/media/header-img.522d5cc4.png"
+  },
+  {
+    "revision": "0345633b50085662435771b32280d437",
+    "url": "/dante-patient-docs-v2/static/media/profile.0345633b.png"
+  },
+  {
+    "revision": "d0443fea9f5ec4620076f48a9b9ad0a6",
+    "url": "/dante-patient-docs-v2/static/media/qr-scan.d0443fea.jpg"
+  },
+  {
+    "revision": "62d57ae5bb5a972b92616bf913e365c3",
+    "url": "/dante-patient-docs-v2/static/media/room_details.62d57ae5.jpg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/dante-patient-docs-v2/static/media/toggler.c275e518.svg"
+  },
+  {
+    "revision": "c15d39d3adde51f2b8abed8ef7d01ff2",
+    "url": "/dante-patient-docs-v2/static/media/user-feedback.c15d39d3.jpg"
+  },
+  {
+    "revision": "5d2fea37b211ae2a0b044761b01adcc0",
+    "url": "/dante-patient-docs-v2/static/media/zoom-in.5d2fea37.jpg"
+  }
+]);
