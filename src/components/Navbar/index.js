c9dfb3dd193c477cceaf1ad7d3204c51b3a93740
Xinhao Liang <xinhao128@hotmail.com>
2019-12-26 17:09:18 -0800

    Full Responsive Layout for Documentation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import Logo from '../../assets/dante-patient-logo.png';
+import { ReactComponent as Toggler } from '../../assets/toggler.svg';
 import './index.scss';
 
 const Navbar = () => {
+  const [display, setDisplay] = useState(false);
   return (
     <div className="nav-layout">
-      <div className="nav-container">
-        <Link to="/" exact>
-          <div className="nav-logo">
-            <img src={Logo} alt="logo" />
+      <div className="nav-col-container">
+        <div className="nav-container">
+          <Link to="/" exact>
+            <div className="nav-logo">
+              <img src={Logo} alt="logo" />
+            </div>
+          </Link>
+          <ul className="nav-menu">
+            <li>
+              <Link to="/" exact className="item" activeClassName="active">Home</Link>
+            </li>
+            <li>
+              <Link to="/documentation" className="item" activeClassName="active">Documentation</Link>
+            </li>
+            <li>
+              <Link to="/faqs" className="item" activeClassName="active">FAQs</Link>
+            </li>
+          </ul>
+          <div className="nav-toggler" onClick={() => setDisplay(!display)}>
+            <Toggler width={30} height={30}/>
           </div>
-        </Link>
-        <ul className="nav-menu">
+        </div>
+        <ul className={"nav-expand-menu " + (display ? 'show' : 'hide')}>
           <li>
-            <Link to="/" exact className="item" activeClassName="active">Home</Link>
+            <Link to="/" exact
+                  className="item"
+                  activeClassName="active"
+                  onClick={() => setDisplay(false)}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/documentation" className="item" activeClassName="active">Documentation</Link>
+            <Link to="/documentation"
+                  className="item"
+                  activeClassName="active"
+                  onClick={() => setDisplay(false)}>
+              Documentation
+            </Link>
           </li>
           <li>
-            <Link to="/faqs" className="item" activeClassName="active">FAQs</Link>
+            <Link to="/faqs"
+                  className="item"
+                  activeClassName="active"
+                  onClick={() => setDisplay(false)}>
+              FAQs
+            </Link>
           </li>
         </ul>
       </div>
